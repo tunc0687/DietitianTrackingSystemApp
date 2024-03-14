ae5f682f39b771d7805c7355fe1553717b09a129
tunc0687 <tunc0687@hotmail.com>
2024-03-15 00:13:02 +0300

    Add DependencyExtension

---
+++ Services/DietitianTrackingSystemApp.Service/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,23 @@
+﻿using DietitianTrackingSystemApp.Data.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietitianTrackingSystemApp.Service.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DietitianTrackingSystemDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Local"));
+            });
+        }
+    }
+}
